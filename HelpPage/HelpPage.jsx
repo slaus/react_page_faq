245de18ed5f01a4890fa15999034bc1d0df71ce8
Vyacheslav <mister.slaus@gmail.com>
2021-11-12 10:54:05 +0200

    Update HelpPage.jsx

--- HelpPage/HelpPage.jsx
+++ HelpPage/HelpPage.jsx
@@ -1,7 +1,4 @@
 import React, {useEffect} from 'react';
-import {useTranslation, Trans} from 'react-i18next';
-import i18next from 'i18next';
-import cookies from 'js-cookie';
 import classNames from 'classnames';
 
 import 'bootstrap/dist/js/bootstrap.js';
@@ -12,50 +9,7 @@ import Button from "../../UI/Button/Button";
 import Icon from "../../UI/Icon/Icon";
 import Accordion from "../../UI/Accordion/Accordion";
 
-const languages = [
-    {
-        code: 'de',
-        name: 'Deutsche',
-        country_code: 'de',
-    },
-    {
-        code: 'en',
-        name: 'English',
-        country_code: 'gb',
-    },
-    {
-        code: 'fr',
-        name: 'Français',
-        country_code: 'fr',
-    },
-    {
-        code: 'es',
-        name: 'Español',
-        country_code: 'es',
-    },
-    {
-        code: 'ar',
-        name: 'العربية',
-        dir: 'rtl',
-        country_code: 'sa',
-    },
-    {
-        code: 'ru',
-        name: 'Русский',
-        country_code: 'ru',
-    },
-];
-
 const HelpPage = () => {
-
-    const currentLanguageCode = cookies.get('i18next') || 'en';
-    const currentLanguage = languages.find((l) => l.code === currentLanguageCode);
-    const {t} = useTranslation();
-
-    useEffect(() => {
-        document.body.dir = currentLanguage.dir || 'ltr';
-        document.title = t('__app_title') + " — " + t('__app_slogan') + "!";
-    }, [currentLanguage, t]);
 
     return (
         <div className="container help">
@@ -65,53 +19,28 @@ const HelpPage = () => {
                         <Trans>__app_handbook</Trans>
                     </div>
                     <div className="help__content--text">
-                        {/*<div className="help__content--text_item">
-                            <div className="question"><Trans>__app_handbook_question_1</Trans></div>
-                            <div className="answer"><Trans>__app_handbook_answer_1</Trans></div>
-                        </div>
-                        <div className="help__content--text_item">
-                            <div className="question"><Trans>__app_handbook_question_2</Trans></div>
-                            <div className="answer"><Trans>__app_handbook_answer_2</Trans></div>
-                        </div>
-                        <div className="help__content--text_item">
-                            <div className="question"><Trans>__app_handbook_question_3</Trans></div>
-                            <div className="answer"><Trans>__app_handbook_answer_3</Trans></div>
-                        </div>
-                        <div className="help__content--text_item">
-                            <div className="question"><Trans>__app_handbook_question_4</Trans></div>
-                            <div className="answer"><Trans>__app_handbook_answer_4</Trans></div>
-                        </div>
-                        <div className="help__content--text_item">
-                            <div className="question"><Trans>__app_handbook_question_5</Trans></div>
-                            <div className="answer"><Trans>__app_handbook_answer_5</Trans></div>
-                        </div>*/}
                         <Accordion
-                            title={<Trans>__app_handbook_question_1</Trans>}
-                            content={i18next.t('__app_handbook_answer_1')}
+                            title="Question No 1"
+                            content="Answer No 1"
                         />
                         <Accordion
-                            title={<Trans>__app_handbook_question_2</Trans>}
-                            content={i18next.t('__app_handbook_answer_2')}
+                            title="Question No 2"
+                            content="Answer No 2"
                         />
                         <Accordion
-                            title={<Trans>__app_handbook_question_3</Trans>}
-                            content={i18next.t('__app_handbook_answer_3')}
+                            title="Question No 3"
+                            content="Answer No 3"
                         />
                         <Accordion
-                            title={<Trans>__app_handbook_question_4</Trans>}
-                            content={i18next.t('__app_handbook_answer_4')}
+                            title="Question No 4"
+                            content="Answer No 4"
                         />
                         <Accordion
-                            title={<Trans>__app_handbook_question_5</Trans>}
-                            content={i18next.t('__app_handbook_answer_5')}
+                            title="Question No 5"
+                            content="Answer No 5"
                         />
                     </div>
                 </div>
-            </div>
-            <div className="help__content--block-back">
-                <Button to="/" transparent>
-                    <Icon name="arrow_left" blue />
-                </Button>
             </div>
         </div>
     );
